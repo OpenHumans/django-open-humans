4f8a73d039f028936bef56cfce2016781d93fd96
Tarannum Khan <tkhaniitr@gmail.com>
2018-06-19 16:34:40 +0530

    list moved to django app

--- openhumans/views.py
+++ openhumans/views.py
@@ -1,3 +1,23 @@
 """
 Create your views here.
 """
+import ohapi
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+class list_files(View):
+
+    success_url = 'main/list.html'
+    not_authorized_url = 'index'
+
+    def get(self, request):
+        """List files."""
+        if request.user.is_authenticated:
+            oh_member = request.user.openhumansmember
+            data = ohapi.api.exchange_oauth2_member(
+                        oh_member.get_access_token())
+            context = {'files': data['data']}
+            return render(request, self.success_url,
+                          context=context)
+        return redirect(self.not_authorized_url)
